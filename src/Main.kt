fa39ac88e79475a2db0e3ebe40b439e5cd444727
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-03 19:12:16 +0300

    added a class and a function in it that calculates the number of necessary ingredients for different numbers of cups of coffee

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,23 @@
 package machine
 
+class CoffeeMachine {
+    private val water = 200
+    private val milk = 50
+    private val coffeeBeans = 15
+
+    fun quantityOfIngredients(cntCups: Int){
+        println("""
+            For $cntCups cups of coffee you will need:
+            ${cntCups * water} ml of water
+            ${cntCups * milk} of milk
+            ${cntCups * coffeeBeans} g of coffee beans
+        """.trimIndent())
+    }
+}
+
 fun main() {
-    println("""
-        Starting to make a coffee
-        Grinding coffee beans
-        Boiling water
-        Mixing boiled water with crushed coffee beans
-        Pouring coffee into the cup
-        Pouring some milk into the cup
-        Coffee is ready!
-    """.trimIndent())
+    println("Write how many cups of coffee you will need:")
+    val cntCups = readln().toInt()
+    val coffeeMachine = CoffeeMachine()
+    coffeeMachine.quantityOfIngredients(cntCups)
 }
