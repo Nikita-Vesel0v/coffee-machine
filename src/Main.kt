690aaeb93597d94c296e03ecd7088482f807bd07
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-03 20:21:33 +0300

    added function that count how much cups are available

--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,64 @@
 package machine
 
-class CoffeeMachine {
-    private val water = 200
-    private val milk = 50
-    private val coffeeBeans = 15
+import kotlin.math.min
 
+class CoffeeMachine {
+    private var totalWater = 0
+    private var totalMilk = 0
+    private var totalBeans = 0
+
+    private val waterPerCup = 200
+    private val milkPerCup = 50
+    private val beansPerCup = 15
+
+    fun addWater(value: Int) {
+        totalWater += value
+    }
+    fun addMilk(value: Int) {
+        totalMilk += value
+    }
+    fun addBeans(value: Int) {
+        totalBeans += value
+    }
     fun quantityOfIngredients(cntCups: Int){
         println("""
             For $cntCups cups of coffee you will need:
-            ${cntCups * water} ml of water
-            ${cntCups * milk} of milk
-            ${cntCups * coffeeBeans} g of coffee beans
+            ${cntCups * waterPerCup} ml of water
+            ${cntCups * milkPerCup} ml of milk
+            ${cntCups * beansPerCup} g of coffee beans
         """.trimIndent())
+    }
+
+    fun availableCups(): Int {
+        val cupsWater = totalWater / waterPerCup
+        val cupsMilk = totalMilk / milkPerCup
+        val cupsBeans = totalBeans / beansPerCup
+        return min(min(cupsWater, cupsMilk), cupsBeans)
     }
 }
 
 fun main() {
-    println("Write how many cups of coffee you will need:")
-    val cntCups = readln().toInt()
     val coffeeMachine = CoffeeMachine()
-    coffeeMachine.quantityOfIngredients(cntCups)
+    //Task 1
+//    println("Write how many cups of coffee you will need:")
+//    val cntCups = readln().toInt()
+//    coffeeMachine.quantityOfIngredients(cntCups)
+
+    //Task 2
+    println("Write how many ml of water the coffee machine has:")
+    coffeeMachine.addWater(readln().toInt())
+    println("Write how many ml of milk the coffee machine has:")
+    coffeeMachine.addMilk(readln().toInt())
+    println("Write how many grams of coffee beans the coffee machine has:")
+    coffeeMachine.addBeans(readln().toInt())
+    println("Write how many cups of coffee you will need:")
+    val needCups = readln().toInt()
+    val availableCups = coffeeMachine.availableCups()
+    println(
+        when {
+            availableCups > needCups -> "Yes, I can make that amount of coffee (and even ${availableCups - needCups} more than that)"
+            availableCups == needCups -> "Yes, I can make that amount of coffee"
+            else -> "No, I can make only $availableCups cups of coffee"
+        }
+    )
 }
