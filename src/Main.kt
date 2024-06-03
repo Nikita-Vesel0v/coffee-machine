9f6f91e218b5d8361dfc2e70fa4e5fcad26e8dc0
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-03 18:51:59 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+package machine
+
+fun main() {
+    println("""
+        Starting to make a coffee
+        Grinding coffee beans
+        Boiling water
+        Mixing boiled water with crushed coffee beans
+        Pouring coffee into the cup
+        Pouring some milk into the cup
+        Coffee is ready!
+    """.trimIndent())
+}
